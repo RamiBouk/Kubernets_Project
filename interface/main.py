cb680ed20061cf2f3c92ad406c6e3edc71c9b991
RamiBouk <ramiboukaroura@gmail.com>
2024-02-15 17:29:51 +0100

    geting a radnoom image serverside

--- interface/main.py
+++ interface/main.py
@@ -1,31 +1,30 @@
-# app.py
-from flask import Flask, render_template, jsonify
-import sqlite3
-import random
+from flask import Flask, render_template, send_file, Response
+import requests
 
 app = Flask(__name__)
-
-def fetch_random_image():
-    conn = sqlite3.connect('image_database.db')
-    cursor = conn.cursor()
-
-    # Fetch a random image
-    cursor.execute('SELECT * FROM images ORDER BY RANDOM() LIMIT 1')
-    row = cursor.fetchone()
-
-    conn.close()
-
-    return row
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/random_image', methods=['GET'])
-def get_random_image():
-    image = fetch_random_image()
-    return jsonify({'image': image})
+@app.route('/api/random_image')
+def random_image():
+    try:
+        # Make a GET request to the server to fetch the random image
+        response = requests.get('http://localhost:5001/api/random_image', stream=True)
+        response.raise_for_status()  # Raise an exception for any error response
+        return Response(response.iter_content(chunk_size=1024), mimetype='image/jpeg')  # Stream the image data directly
+    except requests.exceptions.RequestException as e:
+        print('Error fetching random image:', e)
+        return None
+
+@app.route('/api/classify_image', methods=['POST'])
+def classify_image():
+    # Logic to classify the uploaded image
+    # Access the uploaded file using request.files
+    # Perform classification
+    # Return the results as JSON
+    return jsonify({'result': 'classification_result'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    app.run(debug=True)
