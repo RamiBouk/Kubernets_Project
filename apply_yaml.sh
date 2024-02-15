34eea287cd60e878ea008161b18c2126ad0ac12c
RamiBouk <ramiboukaroura@gmail.com>
2024-02-15 16:57:30 +0100

    adding load balancer and script to apply all yamls

---
+++ apply_yaml.sh
@@ -0,0 +1,5 @@
+kubectl apply -f ./database/database-deployment.yaml
+kubectl apply -f ./classifier/classifier-deployment.yaml
+kubectl apply -f ./interface/interface-deployment.yaml
+kubectl apply -f ./interface/interface-service.yaml
+kubectl apply -f ./load_balancer.yaml
